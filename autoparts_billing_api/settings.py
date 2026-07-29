44dbf9e6e2e37dc0bdd21fc7342d108f56941d81
saklincodes <saklinali022@gmail.com>
2026-07-29 23:38:49 +0530

    fix: replace hardcoded web navigation links with local relative paths inside app

--- autoparts_billing_api/settings.py
+++ autoparts_billing_api/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -120,4 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'stitch_autoparts_billing_pro' / 'stitch_autoparts_billing_pro',
+]
